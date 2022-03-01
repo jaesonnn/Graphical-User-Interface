13e96fcdd7e9f3fb5740d4de7c72feb0984edd76
Floyd Ack <jjjustemailme@gmail.com>
2022-02-28 22:11:49 -0600

    Update chartmenu.h

--- charts-practice/chartmenu.h
+++ charts-practice/chartmenu.h
@@ -1,3 +1,12 @@
+/* Author:      Floyd Ack
+ * Date:        Monday, 28 February 2020
+ * Github:      https://github.com/jaesonnn
+ * Twitter:     https://twitter.com/WellFloyd
+ * Facebook:    https://www.facebook.com/jason.ack/
+ * Instagram:   https://www.instagram.com/wellfloyd_/
+ * LinkedIn:    https://www.linkedin.com/in/floyd-ack-28b5a11a0/
+*/
+
 #ifndef CHARTMENU_H
 #define CHARTMENU_H
 
